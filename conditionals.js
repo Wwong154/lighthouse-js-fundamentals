8fb7d87003daab351291b7462bfe44b0ebc83887
Wilkie Wai-Kit Wong <feanor4d@gmail.com>
2020-11-07 04:43:53 +0000

    Add more conditional examples

--- conditionals.js
+++ conditionals.js
@@ -1,5 +1,6 @@
-/*const raining = true;
-const cold = true;
+
+const raining = true;
+const cold = false;
 
 if (raining) {
   console.log("Don't forget your umbrella!");
@@ -9,17 +10,18 @@ if (cold) {
   console.log("Make sure you pick out a scarf!");
 }
 
-console.log("Now you're ready to go outside!");*/
+console.log("Now you're ready to go outside!");
 
-/*const cold = false;
+//const cold = false;
 
 if (cold) {
   console.log("Make sure you pick out a scarf!");
 } else {
   console.log("Short sleeves are fine.");
 }
-*/
-const temperature = -1;
+
+
+const temperature = -41;
 
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
@@ -30,3 +32,19 @@ if (temperature < 0) {
 }
 
 console.log("Now you're ready to go outside!");
+
+const isCitizen = true;
+const age = 26;
+
+if (isCitizen && age > 18) {
+  console.log("You are eligible to vote.");
+}
+
+if (temperature < -40 || temperature > 40) {
+  console.log("Maybe going outside isn't such a great idea…");
+}
+
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+
